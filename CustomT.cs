a8d4f19c8a4f34c5ddb9845f2b9379f52d250d9b
davidshaek <davidshaek@gmail.com>
2017-10-17 07:50:10 -0500

    Version 1.4 Deber

---
+++ CustomT.cs
@@ -0,0 +1,30 @@
+﻿using EspePocket.Droid;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+
+[assembly: ExportRenderer(typeof(WebView), typeof(CustomT))]
+namespace EspePocket.Droid
+{
+    public class CustomT : Xamarin.Forms.Platform.Android.WebViewRenderer
+    {
+
+        protected override void OnElementChanged(ElementChangedEventArgs<WebView> e)
+        {
+            base.OnElementChanged(e);
+
+            if (Control != null)
+            {
+                Control.Settings.BuiltInZoomControls = true;
+                Control.Settings.DisplayZoomControls = false;
+                Control.Settings.JavaScriptEnabled = true;
+                Control.Settings.UseWideViewPort = true;
+                Control.SetInitialScale(1);
+
+            }
+        }
+
+
+
+    }
+}
